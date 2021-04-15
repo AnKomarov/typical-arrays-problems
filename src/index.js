4713dbcacb1a942028c56ec7f74d3550ec7a3aac
AnKomarov <AnKomarov@korusconsulting.ru>
2021-04-15 20:42:36 +0300

    feat:realiztion of task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if(!(array instanceof Array) || array.length == 0) return 0;
+  const min  = Math.min(...array);
+  return min;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if(!(array instanceof Array) || array.length == 0) return 0;
+  const max  = Math.max(...array);
+  return max;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if(!(array instanceof Array) || array.length == 0) return 0;
+  const sum = array.reduce((prevValue, currentValue) => prevValue + currentValue);
+  const avg = sum / array.length;
+  return avg;
+};
